1cffb80ad3e18792068aefad1112a671f553561e
Mouaz Aldakkak <apothekendatabase@gmail.com>
2024-04-05 11:24:01 +0200

    build: init project and setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './examples/example.mist';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl underline ">Hello world!</h1>
+      <Button radius="lg" color="gray" variant="filled">
+        Click here
+      </Button>
+    </>
+  );
+}
+
+export default App;
